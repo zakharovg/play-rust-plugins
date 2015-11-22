7ab615bbad8b776f5f5a2fa1be3d5bc807a67b81
zakharovg <zakharov.g@gmail.com>
2015-11-22 16:47:50 +0300

    Added plugin with autodescription

---
+++ AirdropSettings/ServerListInfo.cs
@@ -0,0 +1,27 @@
+﻿using Oxide.Core;
+
+namespace Oxide.Plugins
+{
+	[Info("Server List Info", "baton", "1.0.0", ResourceId = 1500)]
+	[Description("Customizable server list information")]
+	public class ServerListInfo : RustPlugin
+	{
+		private void OnServerInitialized()
+		{
+			LoadConfig();
+
+			var headerImage = Config.Get<string>("header");
+			var description = Config.Get<string>("description").Replace("NEWLINE", "\n");
+
+			var rustLib = Interface.Oxide.GetLibrary<Game.Rust.Libraries.Rust>();
+			rustLib.RunServerCommand("server.headerimage", headerImage);
+			rustLib.RunServerCommand("server.description", string.Format("{0}", description));
+		}
+
+		protected override void LoadDefaultConfig()
+		{
+			Config["header"] = string.Empty;
+			Config["description"] = string.Empty;
+		}
+	}
+}
